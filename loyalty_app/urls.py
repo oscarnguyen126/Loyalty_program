5b37e580729d64ffe86e675e91dee13973f15fde
hung_nguyen <oscarnguyen126@gmail.com>
2023-09-20 15:48:42 -0700

    Add category

--- loyalty_app/urls.py
+++ loyalty_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("home/", views.home, name='home'),
+    path("category/", views.list_category, name='category'),
+    path("new_category/", views.new_category, name='new category'),
 ]
