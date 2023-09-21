378a22e8ffd897c1205a535660efc95935879ff4
hung_nguyen <oscarnguyen126@gmail.com>
2023-09-21 14:33:09 -0700

    Modify css for product view

--- loyalty_app/models.py
+++ loyalty_app/models.py
@@ -13,7 +13,6 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.IntegerField("Point exchange", default=0)
-    quantity = models.IntegerField()
     expired_time = models.DateTimeField()
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
