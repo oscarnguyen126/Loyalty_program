6a26ce851c7bad3759f2f787e5d9cc304b806ec5
hung_nguyen <oscarnguyen126@gmail.com>
2023-09-21 17:46:27 -0700

    User profile

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("login/", views.login_user, name='login'),
-    path("logout/", views.logout_user, name='logout'),
-    path("register/", views.register, name='register'),
+    path("login/", views.login_user, name="login"),
+    path("logout/", views.logout_user, name="logout"),
+    path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile"),
+    path("update_profile/", views.update_profile, name="update profile"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
-from . import models
+from .models import User, Province, City, District, Street
 
 
 @csrf_exempt
@@ -34,10 +34,57 @@ def register(request):
         password = make_password(request.POST.get("password"))
         bday = request.POST.get("bday")
         identify_card = request.POST.get("identify_card")
-        user = models.User(
+        user = User(
             username=username, password=password, bday=bday, identify_card=identify_card
         )
         user.save()
         return redirect("login")
 
     return render(request, "register.html")
+
+
+def profile(request):
+    user = User.objects.get(id=request.user.id)
+    return render(request, "profile.html", {"user": user})
+
+
+def update_profile(request):
+    user = User.objects.get(id=request.user.id)
+    provincies = Province.objects.all()
+    cities = City.objects.all()
+    districts = District.objects.all()
+    streets = Street.objects.all()
+
+    if request.method == "POST":
+        firstname = request.POST.get("firstname")
+        lastname = request.POST.get("lastname")
+        identify_card = request.POST.get("identify_card")
+        phone = request.POST.get("phone")
+        mobile = request.POST.get("mobile")
+        province = request.POST.get("province")
+        city = request.POST.get("city")
+        district = request.POST.get("district")
+        street = request.POST.get("street")
+        user.firstname = firstname
+        user.lastname = lastname
+        user.identify_card = identify_card
+        user.phone = phone
+        user.mobile = mobile
+        user.province = province
+        user.city = city
+        user.district = district
+        user.street = street
+        user.save()
+        return redirect("/auth/profile/")
+
+    return render(
+        request,
+        "update_profile.html",
+        {
+            "user": user,
+            "provincies": provincies,
+            "cities": cities,
+            "districts": districts,
+            "streets": streets,
+        },
+    )
